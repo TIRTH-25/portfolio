000adbe9c2ff28c0a1504dd1bb7c0b12755f206f
Your GitHub TIRTH-25 <tirthp2504@gmail.com>
2026-09-11 20:23:24 +0530

    pfp added

--- src/Components/Section1/About.jsx
+++ src/Components/Section1/About.jsx
@@ -1,4 +1,4 @@
-import profile from "../../assets/pfp.jpeg";
+import profile from "../../assets/Pfp.jpg";
 
 const About = () => {
   return (
@@ -9,19 +9,19 @@ const About = () => {
         border border-gray-200
         rounded-xl
         px-4 sm:px-5
-        py-5 sm:py-4
+        py-6 sm:py-5 
         gap-5 sm:gap-8 bg-white"
     >
       <img
-        className="h-36 w-36 sm:h-44 sm:w-44 lg:h-48 lg:w-48 object-cover rounded-2xl shrink-0 border border-gray-100"
+        className="h-36 w-36 sm:h-44 sm:w-44 lg:h-48 lg:w-48 object-cover object-top rounded-2xl shrink-0 border border-gray-100 shadow-xs"
         src={profile}
         alt="Tirth Patel"
       />
-      <div className="text-center sm:text-left">
+      <div className="flex-1 text-center sm:text-left">
         <h2 className="text-2xl sm:text-3xl font-serif mb-2 text-neutral-900">
           About Me
         </h2>
-        <p className="text-sm sm:text-base text-gray-500 leading-relaxed ">
+        <p className="text-sm sm:text-base text-gray-500 leading-relaxed">
           I'm a Full-Stack Developer with a background in Computer Science. I
           have hands-on experience across the MERN stack, .NET, MySQL, and
           Python, and enjoy building clean, engaging web applications. I've also
